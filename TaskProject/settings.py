60d81d35193ba966903224cd04ffc513c520a8e6
KISHOK KUMAR <71526978+kishokkumar22@users.noreply.github.com>
2024-05-07 16:10:02 +0530

    setting changes

--- TaskProject/settings.py
+++ TaskProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "Task",
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,14 @@ WSGI_APPLICATION = "TaskProject.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+   "default": {
+        
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django_db",
+        "USER": "root",
+        "PASSWORD": "MyNewPass",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
